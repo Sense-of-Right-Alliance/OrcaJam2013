16268135d3dff64ed52c82516f7d0edc8481dd72
NICHOLAS PETTYJOHN <n.pettyjohn@gmail.com>
2013-09-06 20:11:20 -0700

    Init Project
    Don't worry about it.

---
+++ Islander/Islander/Islander/Screen/BaseScreen.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace Islander.Screen
+{
+    class BaseScreen
+    {
+        public enum State
+        {
+            None,
+            Running,
+            Finished
+        }
+
+
+        protected Texture2D background;
+        protected ContentManager Content;
+        protected SpriteBatch SpriteBatch;
+        protected int Width;
+        protected int Height;
+        bool initialized = false;
+
+        public BaseScreen()
+        {
+
+        }
+
+        public void Initialize(ContentManager content, SpriteBatch spriteBatch, int width, int height)
+        {
+            initialized = true;
+            Content = content;
+            SpriteBatch = spriteBatch;
+            Width = width;
+            Height = height;
+        }
+
+        protected virtual void LoadContent()
+        {
+
+        }
+
+        protected virtual void UnloadContent()
+        {
+
+        }
+
+        public virtual void Draw()
+        {
+
+        }
+    }
+}
---
+++ Islander/Islander/Islander/Screen/MenuScreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace Islander.Screen
+{
+    class MenuScreen : BaseScreen
+    {
+
+        SpriteFont font;
+
+
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            font = Content.Load<SpriteFont>("Menu");
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+
+
+        }
+
+        protected void DrawString(string text)
+        {
+            Vector2 textDimensions = font.MeasureString(text);
+            SpriteBatch.DrawString(font, text, new Vector2(Width/2 - textDimensions.X, Height/2 - textDimensions.Y), Color.White);
+        }
+
+    }
+}
