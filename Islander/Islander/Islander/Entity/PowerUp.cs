0f87bf1e004b1998d24294380cc08ae2898cc862
Mike <mike_1072@hotmail.com>
2013-09-06 23:26:42 -0700

    fixed merge

--- Islander/Islander/Islander/Entity/PowerUp.cs
+++ Islander/Islander/Islander/Entity/PowerUp.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Graphics;
 
-namespace Islander.Objects
+namespace Islander.Entity
 {
     class PowerUp : Entity
     {
+        public PowerUp(Texture2D sprite) : base(sprite)
+        {
+
+        }
     }
 }
